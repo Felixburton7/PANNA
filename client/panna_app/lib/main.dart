f5628d9231ee6952b76110894ad690cc51e9db69
CasparRaworth <caspar.raworth@hotmail.com>
2025-06-19 17:21:57 +0100

    device preview added

--- client/panna_app/lib/main.dart
+++ client/panna_app/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // Only enable in debug/profile
+      builder: (context) => MyApp(), // Replace MyApp with your app widget
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +18,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
